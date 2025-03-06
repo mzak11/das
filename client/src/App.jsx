d3f37a724130e5ea985218866e6753a528b49db7
mzak11 <mzakwan184@gmail.com>
2025-03-06 13:10:14 +0530

    completed the login and register form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,21 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
- <>
-<div className="bg-dark">
-  <div className="text-sucess">Hello</div>
-</div>
- </>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
