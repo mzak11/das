f90d0727601806ba95048a712164c683f2e980f1
mzak11 <mzakwan184@gmail.com>
2025-03-06 11:49:16 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+ <>
+<div className="bg-dark">
+  <div className="text-sucess">Hello</div>
+</div>
+ </>
+  )
+}
+
+export default App
